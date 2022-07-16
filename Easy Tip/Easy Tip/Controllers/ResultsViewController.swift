e1afcaa5ac0945b07f0e1ee998bc9639d57ef0bc
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-15 22:39:46 -1000

    update

--- Easy Tip/Easy Tip/Controllers/ResultsViewController.swift
+++ Easy Tip/Easy Tip/Controllers/ResultsViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var settingsLabel: UILabel!
+    //@IBOutlet weak var totalLabel: UILabel!
+    //@IBOutlet weak var settingsLabel: UILabel!
     
     var result = "0.0"
     var tip = 10
@@ -20,8 +20,8 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        totalLabel.text = result
-        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+        //totalLabel.text = result
+        //settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
 
     }
     
